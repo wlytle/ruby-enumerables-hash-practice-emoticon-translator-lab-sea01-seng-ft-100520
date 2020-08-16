a1f3f522fe1f8142c9bbb7d1e9ac7b5246f70e0d
Will Lytle <wlytle89@gmail.com>
2020-08-16 17:56:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file_path, emoticon)
   end
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   translator = load_library(file_path)
   translator.each do |i|
     binding.pry
