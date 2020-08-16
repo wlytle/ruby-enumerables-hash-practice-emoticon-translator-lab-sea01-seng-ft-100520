dad0f76adb1242175cf5467136cc48498b20b94c
Will Lytle <wlytle89@gmail.com>
2020-08-16 17:35:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ def load_library(file_path)
         dictionary[key[0].to_sym][:english] = key[1][0]
         dictionary[key[0].to_sym][:japanese] = key[1][1]
     end
+    binding.pry
 end
 
 def get_japanese_emoticon
